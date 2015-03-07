bda4fbfd6196d40d2cefb806432bd37462b49088
PHILIPPE PESCATORE 2 <escortmac@pc8.home>
2015-03-07 21:52:32 +0100

    Initial Commit

---
+++ NePasMourirIdiot/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NePasMourirIdiot
+//
+//  Created by PHILIPPE PESCATORE 2 on 07/03/2015.
+//  Copyright (c) 2015 iAtelier. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
